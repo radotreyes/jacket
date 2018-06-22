b9c1b88d57d37128ef02db3d31f7d6aec4c2e970
Reuben Reyes <reuben.a.reyes@gmail.com`>
2018-06-21 17:06:14 -0700

    install dependencies

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Jacket</h1>
+        <p>Jacket is a health tracking app designed specifically for strength athletes, be they bodybuilders, powerlifters, strongmen, et al.</p>
+      </div>
+    );
+  }
+}
+
+export default App;
